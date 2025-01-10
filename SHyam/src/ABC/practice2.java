5f6537325aa90f9b85268cf9b34ef3c4a5bb2374
Shyam27122024 <testertester1729@gmail.com>
2025-01-10 12:25:02 +0530

    commited by shyam

--- SHyam/src/ABC/practice2.java
+++ SHyam/src/ABC/practice2.java
@@ -19,6 +19,7 @@ public class practice2 {
 	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	    driver.manage().window().maximize();
 	    driver.findElement(By.xpath("//button[text()=' New Transaction']")).click();
+	    driver.findElement(By.xpath("//button[text()=' New Transaction']")).click();
 	    
 	 
 //	    WebElement MobNo = driver.findElement(By.id("txtMobileNumber_KYC"));
